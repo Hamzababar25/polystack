1e008f680e5529c0b4834861ad721f3d9864dd76
Hamza Babar <hamzabava70@gmail.com>
2025-09-10 05:33:15 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,99 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  darkMode: ["selector", "[data-web-theme=dark]"],
+  theme: {
+    container: {
+      center: true,
+      padding: "1.25rem",
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+      colors: {
+        primary: {
+          DEFAULT: "#6b4cd3",
+          color: "#fff",
+          light: {
+            1: "#f9f7ff",
+            2: "#f2efff",
+            3: "#e8e1ff",
+            4: "#dacfff",
+            5: "#cbbcff",
+            6: "#b8a3ff",
+            7: "#a58bff",
+            8: "#8a6ff0",
+            9: "#6b4cd3",
+            10: "#5f44be",
+            11: "#6147c3",
+            12: "#2f265c",
+          },
+          dark: {
+            1: "#1e1a29",
+            2: "#231f31",
+            3: "#2c2840",
+            4: "#332f50",
+            5: "#3a3761",
+            6: "#433f70",
+            7: "#4d4882",
+            8: "#585297",
+            9: "#6b4cd3",
+            10: "#5d43c5",
+            11: "#a99bf3",
+            12: "#ddd6ff",
+          },
+        },
+        body: {
+          light: {
+            1: "#fcfcfd",
+            2: "#f9f9fb",
+            3: "#eff0f3",
+            4: "#e7e8ec",
+            5: "#e0e1e6",
+            6: "#d8d9e0",
+            7: "#cdced7",
+            8: "#b9bbc6",
+            9: "#8b8d98",
+            10: "#80828d",
+            11: "#62636c",
+            12: "#1e1f24",
+          },
+          dark: {
+            1: "#212224",
+            2: "#28292b",
+            3: "#303134",
+            4: "#36373b",
+            5: "#3c3d42",
+            6: "#43444a",
+            7: "#4f5058",
+            8: "#666872",
+            9: "#72747f",
+            10: "#7d7f8a",
+            11: "#b4b6bf",
+            12: "#eeeef0",
+          },
+        },
+      },
+      borderColor: {
+        alpha: {
+          light: "#00073527",
+          dark: "#d6dbfc2f",
+        },
+      },
+      backgroundColor: {
+        body: {
+          striped: {
+            light: "#00005506",
+            dark: "#adc5f30f",
+          },
+        },
+      },
+      boxShadow: {
+        "card-1": "0px 0px 40px 0px rgba(0, 0, 0, 0.08)",
+        "card-2": "0px 10px 20px 0 rgba(0, 0, 0, 0.08)",
+      },
+    },
+  },
+  plugins: [],
+};
